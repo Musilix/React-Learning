a5099bd0cfe47ef408c0f0724af2e919093a071b
Kareem Shehab <kshehab1@student.gsu.edu>
2021-12-11 04:33:17 -0800

    Messing with then handlers

--- JavaScript Learningz/Promises.js
+++ JavaScript Learningz/Promises.js
@@ -24,20 +24,20 @@ let babysFirstPromise = new Promise((resolve, reject) => {
     callback. We can then utilize the .then() suffix for handling success, and parsing through the returned object(s) thereafter
     and also handling any rejections/error from the Promise with catch() 
 */
-babysFirstPromise
-  .then(() => console.log("passed!"))
-  .catch(() => console.log("failed!"));
+// babysFirstPromise
+//   .then(() => console.log("passed!"))
+//   .catch(() => console.log("failed!"));
 
 /*
  We can also utilize the fetch api to make XHR requests
  The fetch() request would in turn return a Promise which would at some point resolve or reject, depending on
  what we sent to the API/server. In a super simple example, there isn't much area for fault, other than Headers.
 */
-console.log("========= Using Fetch =========");
-fetch("https://www.boredapi.com/api/activity")
-  .then((res) => res.json()) // A call to json() actually returns a Promise too, so we would append another .then() onto that!
-  .then((data) => console.log(`Today, you should... ${data.activity}`))
-  .catch((err) => console.error(`There was an issue calling the API: ${err}`));
+// console.log("========= Using Fetch =========");
+// fetch("https://www.boredapi.com/api/activity")
+//   .then((res) => res.json()) // A call to json() actually returns a Promise too, so we would append another .then() onto that!
+//   .then((data) => console.log(`Today, you should... ${data.activity}`))
+//   .catch((err) => console.error(`There was an issue calling the API: ${err}`));
 
 
 /*
@@ -56,3 +56,28 @@ fetch("https://www.boredapi.com/api/activity")
   our code we want to await inside a function labeled as async...  
 */
 
+// Redo-ing Promise chain with Async/Await syntax
+async function getRandomActivity() {
+  try {
+    let apiData = await fetch("https://www.boredapi.com/api/activity");
+    let jsonifiedData = await apiData.json();
+    let randomActivity = jsonifiedData.activity;
+
+    return randomActivity;
+  } catch (e) {
+    console.error(`There was some issue calling bored api: ${e}`);
+  }
+}
+
+let randoAct = await getRandomActivity();
+console.log(`Using async function, we got an activity: ${randoAct}`);
+
+// Testing what's returned from then() and where it goes
+// The final then() on a then chain ends up returning whatever value  
+let randoAct2 = fetch("https://www.boredapi.com/api/activity")
+  .then((res) => res.json())
+  .then((data) => data.activity)
+  .catch((e) => console.error(e));
+
+console.log(randoAct2);
+setTimeout(() => console.log(randoAct2), 5000);
