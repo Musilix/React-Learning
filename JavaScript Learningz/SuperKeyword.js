f931a9103062f75dbc35307c0e26c119f49f5bc0
Kareem Shehab <kshehab1@student.gsu.edu>
2022-01-05 18:10:19 -0800

    Adding more info to super keyword section

--- JavaScript Learningz/SuperKeyword.js
+++ JavaScript Learningz/SuperKeyword.js
@@ -1,6 +1,8 @@
 class Parent{
     constructor(build){
-        this.build = build;
+        // this = {}
+        this.build = build; //this = {build: ...}
+        // return this
     }
 
     showBuild(){
@@ -12,15 +14,23 @@ class Child extends Parent{
     constructor(toy){
         // This line seems to bind everything initiated in the Parent constructor to the childs this context is. Interesting things
         // I wonder how that really works... do we do some type of this.append(super.this)?
+
+        // this = undefined
         super("small");
-        this.toy = toy;
+        //this = {build: ...}
+        this.toy = toy; // this = {build: ..., toy: ...}
+        // return this
     }
 
     // We can reference a parents field by using the this keyword, just as we reference a current context value...
     whoAreYou(){
+        // by the time we call this Child method, the object which is calling it has already been initialized, and therefore
+        // this has been as well. So when this is referenced here, it will point to what is currently stored in the Child instance variable
         console.log(`You have a ${this.build} build and hold a ${this.toy}`)
     }
 }
 
-let child = new Child("toy truck");
+// child = {} at first, and then the engine jumps into the constructor with this new empty object and startds building up the
+// this context within, which it will eventually return after it goes thru it's lifecycle
+let child = new Child("toy truck"); // {build: ..., toy: ..., [[Prototype]]: Child.prototype}
 child.whoAreYou();
